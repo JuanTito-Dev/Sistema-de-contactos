f2a467e04ff6fdcb022bdd50d2dc737db4e6b2ae
JuanJ189 <165536496+JuanJ189@users.noreply.github.com>
2025-03-08 14:41:07 -0400

    Eliminacion del proyecto

--- Sistema_Contactos/Sistema_Contactos/Data/Db_contacto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Proyecto_Bb_2.Data
-{
-    public class Db_contacto : IDb_contacto
-    {
-        public string Db_connection { get; set; } = null!;
-        public string Db_name { get; set; } = null!;
-    }
-}
--- Sistema_Contactos/Sistema_Contactos/Data/IDb_contacto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Proyecto_Bb_2.Data
-{
-    public interface IDb_contacto
-    {
-        string Db_connection { get; set; }
-        string Db_name { get; set; }
-    }
-}
